1896311e140db01f369fb034e7eaddebd68d096b
Zediogo96 <josediogo96@outlook.com>
2025-02-02 18:23:12 +0000

    feat: added price & book now section to details page

--- app/hotel-details/[id]/index.tsx
+++ app/hotel-details/[id]/index.tsx
@@ -1,12 +1,14 @@
 import CheckInOutDetails from '@/components/hotel-details/CheckInOutDetails';
+import Description from '@/components/hotel-details/Description';
 import LocationText from '@/components/hotel-details/LocationText';
 import RatingStars from '@/components/hotel-details/RatingStars';
+import { CURRENCY_SYMBOL_MAP } from '@/constants/currencies';
 import { useHotelById } from '@/services/react-query/hotels';
 
 // * Icons
-import { useLocalSearchParams } from 'expo-router';
+import { Link, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { Dimensions, FlatList, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const { width } = Dimensions.get('window');
 
@@ -15,15 +17,17 @@ const Details = () => {
     const { data: hotel } = useHotelById(id as string);
 
     const renderGalleryItem = ({ item }: { item: string }) => (
-        <Image source={{ uri: item }} style={styles.galleryImage} resizeMode="cover" />
+        <Image source={{ uri: item }} style={s.galleryImage} resizeMode="cover" />
     );
 
     const shouldAllowsScroll = hotel?.gallery && hotel.gallery.length > 1;
 
+    const currencySymbol = CURRENCY_SYMBOL_MAP[hotel?.currency || 'USD'];
+
     if (!hotel) return null;
 
     return (
-        <ScrollView style={styles.container}>
+        <ScrollView style={s.container}>
             <FlatList
                 data={hotel.gallery}
                 keyExtractor={(item) => item}
@@ -32,24 +36,46 @@ const Details = () => {
                 horizontal
                 pagingEnabled
                 showsHorizontalScrollIndicator={false}
-                style={styles.gallery}
+                style={s.gallery}
             />
 
-            <View style={styles.detailsContainer}>
+            <View style={s.detailsContainer}>
                 <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <Text style={styles.hotelName}>{hotel.name}</Text>
+                    <Text style={s.hotelName}>{hotel.name}</Text>
                     <RatingStars count={hotel.stars} />
                 </View>
 
                 <LocationText location={hotel.location} />
 
                 <CheckInOutDetails checkIn={hotel.checkIn} checkOut={hotel.checkOut} />
+
+                <Description />
+            </View>
+
+            <View
+                style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 25, alignItems: 'flex-end' }}
+            >
+                {/* Left section */}
+                <View>
+                    <Text style={{ fontSize: 16, color: '#666', marginBottom: 10 }}>Price</Text>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#333' }}>
+                        {currencySymbol} {hotel.price}
+                        <Text style={{ fontSize: 12, fontWeight: 'normal', color: '#666' }}> / night</Text>
+                    </Text>
+                </View>
+
+                {/* Right section */}
+                <Link href="/modal">
+                    <TouchableOpacity style={s.viewDealButton}>
+                        <Text style={s.textButton}>Book now</Text>
+                    </TouchableOpacity>
+                </Link>
             </View>
         </ScrollView>
     );
 };
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
@@ -75,11 +101,22 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
     },
+    viewDealButton: {
+        flex: 1,
 
-    sectionTitle: {
-        fontSize: 18,
-        fontWeight: '600',
-        marginBottom: 8,
+        backgroundColor: '#000',
+
+        paddingVertical: 12, // Add padding to top and bottom
+        paddingHorizontal: 16, // Add padding to left and right
+
+        borderRadius: 10,
+    },
+
+    textButton: {
+        fontWeight: '500',
+        color: '#fff',
+        fontSize: 15, // Reduced font size
+        textAlign: 'center', // Ensure text is centered if it wraps
     },
 });
 
--- components/hotel-details/Description.tsx
+++ components/hotel-details/Description.tsx
@@ -1,9 +1,36 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Description = () => {
-    
+    return (
+        <View>
+            <Text style={styles.title}>Description</Text>
+            <View style={styles.card}>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl nec justo ultrices efficitur.
+                    Nulla facilisi. Aenean nec nunc auctor, ultricies sapien vitae, ultricies sapien. Nullam in nunc at
+                </Text>
+            </View>
+        </View>
+    );
 };
 
 export default Description;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        fontWeight: '600',
+        marginVertical: 20,
+    },
+    card: {
+        backgroundColor: '#F8F9FA',
+        borderRadius: 12,
+        padding: 16,
+    },
+    text: {
+        fontSize: 14,
+        color: '#666',
+        fontWeight: '400',
+        letterSpacing: 0.5,
+    },
+});
