332965b6f65855dd534ef3057dbf772f022da40f
Zediogo96 <josediogo96@outlook.com>
2025-02-02 12:20:21 +0000

    fetch 3 highest rated hotels hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,10 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-import { useHotels } from '@/services/react-query/hotels';
+import { useHottestHotels } from '@/services/react-query/hotels';
 
 export default function TabOneScreen() {
-    const { data: hotels, isLoading, error } = useHotels();
+    const { data: hotels, isLoading, error } = useHottestHotels();
     console.log('🚀 ~ TabOneScreen ~ hotels:', hotels);
 
     if (isLoading) {
--- services/react-query/hotels.ts
+++ services/react-query/hotels.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_CACHE_TIME } from '@/constants/react-query';
 import { useQuery } from '@tanstack/react-query';
 import { fetchHotels } from '../api/hotels';
 
@@ -5,6 +6,15 @@ export const useHotels = () => {
     return useQuery({
         queryKey: ['hotels'],
         queryFn: fetchHotels,
-        staleTime: 1000 * 60 * 5, // Cache for 5 minutes
+        staleTime: DEFAULT_CACHE_TIME,
+    });
+};
+
+export const useHottestHotels = () => {
+    return useQuery({
+        queryKey: ['hotels', 'hottest'],
+        queryFn: fetchHotels,
+        staleTime: DEFAULT_CACHE_TIME,
+        select: (hotels) => hotels.sort((a, b) => b.userRating - a.userRating).slice(0, 3),
     });
 };
